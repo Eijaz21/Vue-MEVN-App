29561aeb9106e8e8b9ec4b2c352eb144b8b8e6f3
Naim <nbaganee@gmail.com>
2020-07-28 16:21:11 +0400

    completed create new data form

--- src/components/CreateComponent.vue
+++ src/components/CreateComponent.vue
@@ -4,7 +4,29 @@
 
         <div class="col-md-6">
             <!-- Content goes here -->
+            <h3 class="text-center">Create Students</h3>
 
+            <form @submit.prevent="handleSubmitForm">
+
+                <div class="form-group">
+                    <label for="name">Name</label>
+                    <input type="text" name="" id="" class="form-control" v-model="student.name" required>
+                </div>
+
+                <div class="form-group">
+                    <label for="email">Email</label>
+                    <input type="email" name="" id="" class="form-control" v-model="student.email" required>
+                </div>
+
+                <div class="form-group">
+                    <label for="phone">Phone</label>
+                    <input type="text" name="" id="" class="form-control" v-model="student.phone" required>
+                </div>
+
+                <div class="form-group">
+                   <button class="btn btn-danger btn-block">Create</button>
+                </div>
+            </form>
         </div>
 
     </div>
@@ -15,8 +37,17 @@
     export default {
       data() {
           return {
-              
+            student: {
+                name: '',
+                email: '',
+                phone: ''
+            }
           }
+      },
+      methods: {
+        handleSubmitForm() {
+            
+        }
       }  
     }
 </script>
